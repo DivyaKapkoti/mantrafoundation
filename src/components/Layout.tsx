49cb2630c2d082fe899fe06ab83a191041af4c49
DivyaKapkoti <divyakapkoti39@gmail.com>
2026-06-28 20:50:30 +0530

    fix navbar spacer all pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,10 @@ import { SiteFooter } from "./SiteFooter";
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <SiteHeader />
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
-    </div>
+  <SiteHeader />
+  <div className="h-20 sm:h-24" />
+  <main className="flex-1">{children}</main>
+  <SiteFooter />
+</div>
   );
 }
