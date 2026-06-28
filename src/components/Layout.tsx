f4df0046b2a24499ed4bac1815863014244ec667
DivyaKapkoti <divyakapkoti39@gmail.com>
2026-06-28 19:44:54 +0530

    fix navbar overlay

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <SiteHeader />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 pt-16 sm:pt-20">{children}</main>
       <SiteFooter />
     </div>
   );
